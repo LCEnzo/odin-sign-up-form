d66fe09e56d14865e2c6e64c113d58181a24a906
LCEnzo <lcenzo@protonmail.ch>
2022-03-24 16:51:08 +0100

    Added placeholder values for inputs. Link colors adjusted. Added logic to check that password and password confirmation fields are the same. TODO, disallow submitting if errors for password, add hover box for password length info.

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const passField = document.querySelector("input#password")
+const confirmPassField = document.querySelector("input#confirm-password")
+
+const minimumPasswordLength = 8
+const passwordErrorClassName = "error"
+
+function checkPasswordConfirmation(event) {
+    let password = passField.value 
+    let confirmation = confirmPassField.value 
+
+    if(confirmation.length == 0 || password == confirmation) {
+        if(password.length >= 8) passField.classList.remove(passwordErrorClassName)
+        confirmPassField.classList.remove(passwordErrorClassName)
+    } else {
+        passField.classList.add(passwordErrorClassName)
+        confirmPassField.classList.add(passwordErrorClassName)
+    }
+
+    if(password.length < 8) {
+        passField.classList.add(passwordErrorClassName)
+        
+        // TODO add hovering box instead of creating a alert
+        alert("password length is too low, minumum is 8 characters")
+    }
+}
+
+passField.addEventListener('focusout', checkPasswordConfirmation)
+confirmPassField.addEventListener('focusout', checkPasswordConfirmation)
