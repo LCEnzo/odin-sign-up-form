e131e444658f03a4349fafb35aa8e55dfe9ee016
Luka Colic <lcenzo@protonmail.ch>
2022-04-03 16:34:37 +0200

    Finished password validation.

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,53 @@
 const passField = document.querySelector("input#password")
 const confirmPassField = document.querySelector("input#confirm-password")
+const submit = document.querySelector("#create-account-button")
+const form  = document.getElementsByTagName('form')[0];
+const inputList = document.querySelectorAll(".input-container > input")
 
-const minimumPasswordLength = 8
-const passwordErrorClassName = "error"
-
-function checkPasswordConfirmation(event) {
-    let password = passField.value 
-    let confirmation = confirmPassField.value 
-
-    if(confirmation.length == 0 || password == confirmation) {
-        /*if(password.length >= 8)*/ passField.classList.remove(passwordErrorClassName)
-        confirmPassField.classList.remove(passwordErrorClassName)
-    } else {
-        passField.classList.add(passwordErrorClassName)
-        confirmPassField.classList.add(passwordErrorClassName)
-    }
-    /*
-    if(password.length < 8) {
-        passField.classList.add(passwordErrorClassName)
-        
-        // TODO add hovering box instead of creating a alert
-        alert("password length is too low, minumum is 8 characters")
-    }*/
+function showSubmitErrMsg(text) {
+    const errMsg = document.querySelector("#submit-error-message")
+    errMsg.hidden = false
+    errMsg.innerHTML = text
 }
 
-passField.addEventListener('focusout', checkPasswordConfirmation)
-confirmPassField.addEventListener('focusout', checkPasswordConfirmation)
+function hideSubmitErrMsg() {
+    const errMsg = document.querySelector("#submit-error-message")
+    let attr = document.createAttribute('hidden');
+    errMsg.hidden = true
+}
+
+const minimumPasswordLength = parseInt(passField.attributes["minlength"].value)
+const passwordErrorClassName = "inputError"
+
+let el = ""
+function checkValidity(event) {
+    const target = event.target
+
+    event.stopPropagation();
+
+    if(target.checkValidity()) {
+        target.classList.remove(passwordErrorClassName)
+    } else {
+        target.classList.add(passwordErrorClassName)
+    }
+
+    if(target === passField || target === confirmPassField) {
+        if(passField.value.length == 0 && confirmPassField.value.length === 0) {
+            passField.classList.remove(passwordErrorClassName)
+            confirmPassField.classList.remove(passwordErrorClassName)
+            hideSubmitErrMsg()
+        } else if(target.checkValidity() && (passField.value === confirmPassField.value)) {
+            passField.classList.remove(passwordErrorClassName)
+            confirmPassField.classList.remove(passwordErrorClassName)
+            hideSubmitErrMsg()
+        } else if(confirmPassField.value !== "") {
+            passField.classList.add(passwordErrorClassName)
+            confirmPassField.classList.add(passwordErrorClassName)
+            showSubmitErrMsg("Password and confirm password fields do not match.")
+        }
+    }
+}
+
+for(let node of inputList) {
+    node.addEventListener('input', checkValidity)
+}
